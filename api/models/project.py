4994d34de8a66eac281e5350a58e0dfcdbc39570
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-04-10 10:06:39 -0400

    create project model

---
+++ api/models/project.py
@@ -0,0 +1,17 @@
+from api.models.db import db
+from datetime import datetime
+
+class Project(db.Model):
+  __tablename__ = 'projects'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  deadline = db.Column(db.Date)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+  profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+
+def __repr__(self):
+  return f"Project('{self.id}', '{self.name}'"
+
+def serialize(self):
+  project = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+  return project
